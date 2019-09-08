81917ae63c703f317f9a1ec61cefebf59604824d
bodrovphone <bodrovphone@gmail.com>
2019-09-08 21:53:34 +0300

    added some pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Link from 'next/link';
 
+const Post = props => (
+  <li>
+    <Link as={`/${props.slug}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
 export default () => (
   <div>
-    <p>Hello World!</p>
-    <Link href="/contact">
-      <a>Contact me!</a>
-    </Link>
+    <h2>My blog!</h2>
+    <ul>
+      <li>
+        <Post slug="yet-another-post" title="Yet another post" />
+        <Post slug="seond-post" title="Second post" />
+        <Post slug="hello-world" title="Hello world!" />
+      </li>
+    </ul>
   </div>
 );
