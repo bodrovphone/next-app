bc9013497c59debcbb0364590e23c26065fb8c1f
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2019-12-09 14:42:32 +0200

    dynamic pages as query (withRoutes)

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+import axios from "axios";
 import BaseLayout from "../components/layouts/BaseLayout";
+import Link from "next/link";
 
 export default class Portfolios extends Component {
+  static async getInitialProps() {
+    let posts;
+    try {
+      posts = await axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.data.splice(0, 10));
+    } catch (err) {
+      err => console.log(err);
+    }
+    return { posts };
+  }
+
+  renderPosts = posts => {
+    return posts.map(post => (
+      <li key={post.id + Math.random()}>
+        <Link href={`/portfolio?title=${post.title}`}>
+          <a style={{ fontSize: "20px" }}>{post.title}</a>
+        </Link>
+      </li>
+    ));
+  };
+
   render() {
+    const { posts } = this.props;
     return (
       <BaseLayout>
         <h1>I am Portfolios Page</h1>
+        <ul>{this.renderPosts(posts)}</ul>
       </BaseLayout>
     );
   }
