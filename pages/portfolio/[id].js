7f2dc5af731299d0ec04a01995153b4a4cf8fabf
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2019-12-10 12:43:50 +0200

    single portfolio API

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -4,11 +4,25 @@ import BaseLayout from "../../components/layouts/BaseLayout";
 import { withRouter } from "next/router";
 
 class Portfolio extends Component {
+  static async getInitialProps(context) {
+    let post;
+    try {
+      post = await axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${context.query.id}`)
+        .then(response => response.data);
+    } catch (err) {
+      err => console.log(err);
+    }
+    return { post };
+  }
+
   render() {
+    const { post } = this.props;
     return (
       <BaseLayout>
-        <h1>I am Portfolios Page</h1>
-        <h2>{this.props.router.query.id}</h2>
+        <h1>I am [id].js page with the id of {post.id}</h1>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
       </BaseLayout>
     );
   }
