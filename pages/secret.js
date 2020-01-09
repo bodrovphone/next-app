935ac6978692c8a47106421fa0b3b517a18bfa84
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-01-09 16:12:03 +0200

    expose an API to get secret info

--- pages/secret.js
+++ pages/secret.js
@@ -2,13 +2,40 @@ import React, { Component } from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/BasePage";
 import withAuth from "../components/hoc/withAuth";
+import axios from "axios";
 
 class Secret extends Component {
+  constructor() {
+    super();
+    this.state = {
+      secretData: []
+    };
+  }
   static getInitialProps() {
     const superSecretValue = "pizdec";
 
     return { superSecretValue };
   }
+
+  async componentDidMount() {
+    const res = await axios.get("/api/v1/secret");
+    const secretData = res.data;
+    this.setState({ secretData });
+  }
+
+  displaySecretData = () => {
+    const { secretData } = this.state;
+    if (secretData && secretData.length) {
+      return secretData.map((item, index) => {
+        return (
+          <>
+            <p key={index + "-title"}>{item.title}</p>
+            <p key={index + "-desc"}>{item.description}</p>
+          </>
+        );
+      });
+    }
+  };
 
   render() {
     const { superSecretValue } = this.props;
@@ -17,6 +44,7 @@ class Secret extends Component {
         <BasePage>
           <h1>I am Secret Page</h1>
           <h2>{superSecretValue}</h2>
+          {this.displaySecretData()}
         </BasePage>
       </BaseLayout>
     );
--- server/index.js
+++ server/index.js
@@ -5,10 +5,19 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const secretData = [
+  { title: "SecretData 1", description: "Plans how to build spaceship" },
+  { title: "SecretData 2", description: "My secret data" }
+];
+
 app
   .prepare()
   .then(() => {
     const server = express();
+
+    server.get("/api/v1/secret", (req, res) => {
+      return res.json(secretData);
+    });
 
     server.get("*", (req, res) => {
       return handle(req, res);
