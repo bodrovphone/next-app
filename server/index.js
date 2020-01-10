cfb0ee1c4149f41a02d08751eb8e610f020437bc
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-01-10 17:32:31 +0200

    server service with jwt checks

--- server/index.js
+++ server/index.js
@@ -27,6 +27,14 @@ app
       return handle(req, res);
     });
 
+    server.use(function(err, req, res, next) {
+      if (err.name === "UnauthorizedError") {
+        res
+          .status(401)
+          .send({ title: "invalid token...", detail: "unathorized access..." });
+      }
+    });
+
     server.use(handle).listen(3000, err => {
       if (err) throw err;
       console.log("> Ready on http://localhost:3000");
--- server/services/auth.js
+++ server/services/auth.js
@@ -1,10 +1,12 @@
-exports.checkJWT = function(req, res, next) {
-  const isValid = false;
-  if (isValid) {
-    next();
-  } else {
-    return res
-      .status(401)
-      .send({ title: "Not Auhtorized", details: "fuck off" });
-  }
-};
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+exports.checkJWT = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    jwksUri: "https://dev-4vh4q7if.eu.auth0.com/.well-known/jwks.json"
+  }),
+  audience: "Yp4oRNsPjRxBSVyZRzTm8B6sefH382Zx",
+  issuer: "https://dev-4vh4q7if.eu.auth0.com/",
+  algorithms: ["RS256"]
+});
