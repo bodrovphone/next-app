7307ecce37e339fffcd321e11864b076ecccd4ba
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-01-10 16:33:55 +0200

    create server service with middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const next = require("next");
 
+// SERVICES
+
+const authService = require("./services/auth");
+
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -15,7 +19,7 @@ app
   .then(() => {
     const server = express();
 
-    server.get("/api/v1/secret", (req, res) => {
+    server.get("/api/v1/secret", authService.checkJWT, (req, res) => {
       return res.json(secretData);
     });
 
