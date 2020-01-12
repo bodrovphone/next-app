a0b81b6caccf1202253474c6cc67625dc4d5186d
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-01-12 13:46:03 +0200

    begin authorization roles implementation

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -2,7 +2,9 @@ import React from "react";
 import BaseLayout from "../layouts/BaseLayout";
 import BasePage from "../BasePage";
 
-const withAuth = Component => {
+const nameSpace = "http://localhost:3000/";
+
+const withAuth = (Component, role) => {
   return class withAuth extends React.Component {
     static async getInitialProps(args) {
       const pageProps =
@@ -11,7 +13,8 @@ const withAuth = Component => {
       return { ...pageProps };
     }
     renderConditional = () => {
-      const { isAuthenticated } = this.props.auth;
+      const { isAuthenticated, user } = this.props.auth;
+      const userRole = user?.[`${nameSpace}role${role}`];
       if (isAuthenticated) {
         return <Component {...this.props} />;
       } else {
