8927504d7f1e28c41ae9103aba228ff6718a1ab6
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-02-04 15:48:47 +0200

    Book model and mongo db Restful API

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const next = require("next");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // SERVICES
 
 const authService = require("./services/auth");
 
 const config = require("./config");
+const Book = require("./models/book");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -28,6 +30,62 @@ app
   .prepare()
   .then(() => {
     const server = express();
+    server.use(bodyParser.json());
+
+    server.post("/api/v1/books", (req, res) => {
+      const bookData = req.body;
+
+      const book = new Book(bookData);
+
+      book.save((err, createdBook) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+        return res.json(createdBook);
+      });
+    });
+
+    server.get("/api/v1/books", (req, res) => {
+      Book.find({}, (err, allBooks) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        return res.json(allBooks);
+      });
+    });
+
+    server.patch("/api/v1/books/:id", (req, res) => {
+      const bookId = req.params.id;
+      const bookData = req.body;
+
+      Book.findById(bookId, (err, foundBook) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        foundBook.set(bookData);
+        foundBook.save((err, savedBook) => {
+          if (err) {
+            return res.status(422).send(err);
+          }
+
+          return res.json(savedBook);
+        });
+      });
+    });
+
+    server.delete("/api/v1/books/:id", (req, res) => {
+      const bookId = req.params.id;
+
+      Book.deleteOne({ _id: bookId }, (err, deletedBook) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        return res.json({ status: "DELETED" });
+      });
+    });
 
     server.get("/api/v1/secret", authService.checkJWT, (req, res) => {
       return res.json(secretData);
